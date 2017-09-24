3d503cc6f3a6a8cae53b14503e0b0b4f9593db6d
Joe Kington <joferkington@gmail.com>
2017-09-24 14:49:45 +0000

    Add training observations points and class

---
+++ create_training_data.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# We're going to cheat a bit...
+did_not_flood = np.loadtxt('data/did_not_flood.txt')
+all_flooded = np.load('data/raw_verts.npy')
+
+# This is the cheating part. Get a random subset of the flooded vertices the
+# same size as our manually picked did_no_flood points. We won't really have a
+# true "test" dataset at all, but that's okay-ish in this case...
+n = len(did_not_flood)
+idx = np.random.randint(0, len(all_flooded), n)
+train_flooded = all_flooded[idx, :]
+
+flooded = np.hstack([np.zeros(n), np.ones(n)])
+xy = np.vstack([did_not_flood, train_flooded])
+
+np.savetxt('data/train_xy.txt', xy)
+np.savetxt('data/train_class.txt', flooded)
